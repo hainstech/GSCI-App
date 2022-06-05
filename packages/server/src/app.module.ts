b3103bfc56b8345a52aaac3e1aa209e027f2b16e
Dominic Hains <contact.dominichains@gmail.com>
2022-06-05 12:48:11 -0400

    build: environment variables

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.production'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
