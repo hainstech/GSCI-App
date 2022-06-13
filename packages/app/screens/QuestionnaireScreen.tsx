c0ecabc1d6d2886b5b1ae8c10bea844769ff3f2e
Dominic Hains <contact.dominichains@gmail.com>
2022-06-13 18:23:26 -0400

    feat: questionnaire display

--- packages/app/screens/QuestionnaireScreen.tsx
+++ packages/app/screens/QuestionnaireScreen.tsx
@@ -1,16 +1,36 @@
-import { useRoute } from '@react-navigation/native';
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef } from 'react';
 import { Container } from '../components/Container';
 import { RootStackScreenProps } from '../types';
+import ReactNativeForm, { defaultProps, FormContext } from 'rjsf-native';
+import { Alert, Button } from 'react-native';
 
 export default function QuestionnaireScreen({
   route,
 }: RootStackScreenProps<'Questionnaire'>) {
   const { questionnaire } = route.params;
+  const form = useRef<any>(null);
   return (
     <Container>
-      <Text>{questionnaire.title}</Text>
+      <ReactNativeForm
+        // @ts-ignore
+        ref={form}
+        onError={(e) => {
+          Alert.alert('Error', 'Please fill in all the fields');
+        }}
+        schema={questionnaire.schema}
+        uiSchema={questionnaire.uischema}
+        onSubmit={(form) => {
+          const { formData } = form;
+          console.log(formData);
+        }}
+      >
+        <Button
+          title="Submit"
+          onPress={() => {
+            form.current?.submit();
+          }}
+        />
+      </ReactNativeForm>
     </Container>
   );
 }
