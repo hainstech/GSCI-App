b189220323ccd542e1f7c5ad09c7c0fd04ca4480
Dominic Hains <contact.dominichains@gmail.com>
2022-06-18 23:26:59 -0400

    feat: netinfo event listener

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import NetInfo from '@react-native-community/netinfo';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { Alert } from 'react-native';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    NetInfo.addEventListener((connection) => {
+      if (connection.type === 'none') {
+        Alert.alert('No Internet Connection', 'Please connect to the internet');
+      }
+    });
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
